1c895d953e6e88fb8f2707282b58c4defa7f0265
Aryan Naraghi <aryan.naraghi@gmail.com>
2014-06-15 18:10:08 -0700

    Adds a handler for upgrading HTTP connections to WebSocket connections. Also adds a small JavaScript snippet to test the upgrading.

--- paintserver.go
+++ paintserver.go
@@ -3,7 +3,15 @@ package main
 import (
   "net/http"
   "html/template"
+  "log"
+
+  "github.com/gorilla/websocket"
 )
+
+var websocketUpgrader = websocket.Upgrader{
+  ReadBufferSize:  1024,
+  WriteBufferSize: 1024,
+}
 
 var templates = template.Must(template.ParseFiles("index.html"))
 
@@ -15,7 +23,17 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
   }
 }
 
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+  _, err := websocketUpgrader.Upgrade(w, r, nil)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  log.Printf("HTTP connection upgraded to WebSocket connection: %v\n", r)
+}
+
 func main() {
   http.HandleFunc("/", indexHandler)
+  http.HandleFunc("/echo/", echoHandler)
   http.ListenAndServe(":8080", nil)
 }
